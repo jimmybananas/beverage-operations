3d9edc90508cf1aca1b25cbef7ab82d689e99779
James Pederson <jspederson@Jamess-MacBook-Air-2.local>
2025-05-06 21:46:22 -0600

    Initial Commit

---
+++ src/main/java/com/jspsolutions/beverageoperations/BeverageoperationsApplication.java
@@ -0,0 +1,13 @@
+package com.jspsolutions.beverageoperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeverageoperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeverageoperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jspsolutions/beverageoperations/BeverageoperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jspsolutions.beverageoperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeverageoperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
